dc10f5d0da75136fe98ef507d5d158c85d79cf22
Bohdan Kripak <bohdan.kripak@gmail.com>
2018-01-22 16:00:15 +0200

    New version of Splitter.

--- splitter/contracts/Splitter.sol
+++ splitter/contracts/Splitter.sol
@@ -6,19 +6,55 @@ contract Splitter {
     address secondRecipient;
     address owner;
     
-    function Splitter(address recipient1, address recipient2) public payable {
+    bool onePayoutPerformed;
+    
+    function Splitter(address recipient1, address recipient2) public {
+        require(recipient1 != address(0));
+        require(recipient2 != address(0));
         firstRecipient = recipient1;
         secondRecipient = recipient2;
         owner = msg.sender;
     }
     
-    function refill() public payable {
-        firstRecipient.transfer(msg.value/2);
-        secondRecipient.transfer(msg.value/2);
+    function refill() public payable payoutNotPerformedYet() returns (uint256) {
+        return getBalance();
     }
     
-    function kill() public isOwner() {
+    function performPayout() public isOwner() notEmpty() returns(address sendTo, uint256 payoutSumm) {
+        if (!onePayoutPerformed) {
+            uint256 halfOfBalance = this.balance/2;
+            require(firstRecipient.send(halfOfBalance));
+            onePayoutPerformed = true;
+            return (firstRecipient, halfOfBalance);
+        } else {
+            uint256 restOfBalance = this.balance;
+            require(secondRecipient.send(restOfBalance));
+            onePayoutPerformed = false;
+            return (secondRecipient, restOfBalance);
+        }
+    }
+    
+    function kill() public isOwner() isEmpty() {
         selfdestruct(owner);
+    }
+    
+    function getBalance() public constant returns(uint256) {
+        return this.balance;
+    }
+    
+    modifier payoutNotPerformedYet() {
+        require(onePayoutPerformed == false);
+        _;
+    }
+    
+    modifier notEmpty() {
+        require(this.balance > 0);
+        _;
+    }
+    
+    modifier isEmpty() {
+        require(this.balance == 0);
+        _;
     }
     
     modifier isOwner() {
